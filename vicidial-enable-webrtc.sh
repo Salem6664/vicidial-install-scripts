e92ba373b3c8727c8349de0bb026e46b5e9068f9
Salem6664 <37146559+Salem6664@users.noreply.github.com>
2024-11-15 00:24:52 +0800

    Update vicidial-enable-webrtc.sh

--- vicidial-enable-webrtc.sh
+++ vicidial-enable-webrtc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Download CyburPhone"
+echo "Download ViciPhone"
 cd /var/www/html
-git clone https://github.com/carpenox/CyburPhone.git
-chmod -R 744 CyburPhone
-chown -R apache:apache CyburPhone
+git clone https://github.com/vicimikec/ViciPhone.git
+chmod -R 744 ViciPhone
+chown -R apache:apache ViciPhone
 
 echo "Install certbot for LetsEncrypt"
 if [ -f /etc/redhat-release ]; then
@@ -41,7 +41,7 @@ mysql -e "use asterisk; update servers set web_socket_url='wss://$DOMAINNAME:808
 
 echo "Add DOMAINAME system_settings webphone_url"
 echo "%%%%%%%%%%%%%%%This Wont work if you SET root Password%%%%%%%%%%%%%%%"
-mysql -e "use asterisk; update system_settings set webphone_url='https://$DOMAINNAME/CyburPhone/cyburphone.php';"
+mysql -e "use asterisk; update system_settings set webphone_url='https://$DOMAINNAME/viciphone/viciphone.php';"
 
 echo "update the SIP_generic"
 mysql -e "use asterisk; update vicidial_conf_templates set template_contents='type=friend 
